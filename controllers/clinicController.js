9192e85acdd04c7f3353003d57373381993615ea
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2025-04-16 14:57:20 +0800

    Initial Commit

--- controllers/clinicController.js
+++ controllers/clinicController.js
@@ -12,7 +12,7 @@ exports.createClinic = async (req, res, next) => {
       address, 
       longitude = req.body.location.coordinates.longitude, 
       latitude = req.body.location.coordinates.latitude, 
-      contactNumber, 
+      contactNumber = req.body.contactNumber.replace(/\s+/g, ''), 
       email, 
       openingTime = req.body.operatingHours.openingTime, 
       closingTime = req.body.operatingHours.closingTime,
