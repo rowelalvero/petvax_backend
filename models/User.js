29be8e414dcfdc3e946cdec5cc70079d6c5a29c6
rowelalvero <alvero.323400@sjdelmonte.sti.edu.ph>
2025-04-18 19:30:44 +0800

    Added clinic owner in clinic creation

--- models/User.js
+++ models/User.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password is required'],
     minlength: [8, 'Password must be at least 8 characters'],
-    select: false // Never return password in queries
+    select: true // Never return password in queries
   },
   phoneNumber: {
     type: String,
